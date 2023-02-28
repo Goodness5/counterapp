2c5665466a497118b8cd517330d0baf984059d15
superman <goodnesskolapo@gmail.com>
2023-02-28 20:42:47 +0100

    merged conflicts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
-=======
 import React from 'react';
 // import logo from './logo.svg';
 import { useState } from "react";
@@ -42,6 +21,5 @@ const App= () => {
       </div>
     );
   }
->>>>>>> f5db3c0be289df28d1c78d85311e56c1c31dac69
 
 export default App;
